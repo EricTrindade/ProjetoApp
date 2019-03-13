885a13e1a631807adadfd858f6e2eecc65f79bcd
ERIC TRINDADE FERREIRA <26146045@sempreunama.com.br>
2019-03-13 16:51:59 -0300

    Initial commit

---
+++ app/src/main/java/com/example/a26146045/aula13_03/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.a26146045.aula13_03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RadioGroup sexo;
+    private EditText altura;
+    double resultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sexo=findViewById(R.id.sexor);
+        altura=findViewById(R.id.alturar);
+    }
+
+    public void calcularPesoIdeal(View sc){
+        int opcao=sexo.getCheckedRadioButtonId( );
+
+        if (opcao==R.id.RBF){
+            double alt = Double.parseDouble(altura.getText().toString());
+            resultado= (alt*62.1)-44.7;
+
+        }else{
+            double alt = Double.parseDouble(altura.getText().toString());
+            resultado= (alt*72.7)-58;
+
+        }
+
+    }
+}
